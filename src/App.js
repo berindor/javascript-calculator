00cfbfcd8d1d865286cffca57a0d616cb32d5351
Beringer Dorottya <beringerdorottya@gmail.com>
2023-11-29 17:08:14 +0100

    add input handler

--- src/App.js
+++ src/App.js
@@ -1,60 +1,106 @@
-
 import React from 'react';
 import './App.css';
 
+const buttonConfig = [
+  { id: 'zero', html: 0 },
+  { id: 'one', html: 1 },
+  { id: 'two', html: 2 },
+  { id: 'three', html: 3 },
+  { id: 'four', html: 4 },
+  { id: 'five', html: 5 },
+  { id: 'six', html: 6 },
+  { id: 'seven', html: 7 },
+  { id: 'eight', html: 8 },
+  { id: 'nine', html: 9 },
+  { id: 'decimal', html: '.' },
+  { id: 'add', html: '+' },
+  { id: 'subtract', html: '-' },
+  { id: 'multiply', html: 'x' },
+  { id: 'divide', html: '/' },
+  { id: 'clear', html: 'C' },
+  { id: 'equals', html: '=' }
+];
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      displayFirstRow: 'This is the display.'
-    }
+      displayArr: [],
+      firstRow: 'This is the display.',
+      secondRow: 'This is the result.'
+    };
+    this.createButton = this.createButton.bind(this);
+    this.handleArr = this.handleArr.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(button) {
-    switch (button) {
-      case "one":
-      case "two":
-      case "three":
-        this.setState({displayFirstRow: button});
-        break;
-      default: 
-      this.setState({displayFirstRow: 'Clicked something else'});
-    }
+  createButton({ id, html }) {
+    return (
+      <button id={id} key={id} onClick={() => this.handleClick(html)}>
+        {html}
+      </button>
+    );
   }
 
-  componentDidMount() {
-    document.addEventListener("click", (PointerEvent) => {
-      this.handleClick(PointerEvent.target.id);
+  handleArr(button, arr) {
+    const operators = ['+', '-', 'x', '/'];
+    if (operators.includes(button) && button === arr[arr.length - 1]) {
+      return arr;
+    }
+    if (operators.includes(button) && button !== '-' && operators.includes(arr[arr.length - 2]) && arr[arr.length - 1] === '-') {
+      //[+x/] after [+-x/], - => remove both, add new
+      arr.splice(arr.length - 2, 2, button);
+      return arr;
+    }
+    if (operators.includes(button) && button !== '-' && operators.includes(arr[arr.length - 1])) {
+      //[+x/] after [+-x/] => remove, add new
+      arr[arr.length - 1] = button;
+      return arr;
+    }
+    if (button === 0 && operators.includes(arr[arr.length - 2]) && arr[arr.length - 1] === 0) {
+      // 0 after [3-x/], 0 => do nothing
+      return arr;
+    }
+    if (button === '.' && arr.findLast(element => typeof element === 'string') === '.') {
+      //max one decimal point in a number
+      return arr;
+    }
+    if (button === '.' && operators.includes(arr[arr.length - 1])) {
+      arr.push(0, '.');
+      return arr;
+    }
+    arr.push(button);
+    return arr;
+  }
+
+  handleClick(button) {
+    if (button === 'C') {
+      this.setState({
+        displayArr: [],
+        firstRow: '',
+        secondRow: 0
+      });
+      return;
+    }
+    const arr = this.handleArr(button, this.state.displayArr);
+    this.setState({
+      displayArr: arr,
+      firstRow: arr.join('')
     });
-  } 
+  }
 
   render() {
+    const buttons = buttonConfig.map(buttonInfo => this.createButton(buttonInfo));
+
     return (
       <div id="calculator">
-        <div id="display">{this.state.displayFirstRow}</div>
-        <div id="key-wrapper">
-          <button id="zero">0</button>
-          <button id="one">1</button>
-          <button id="two">2</button>
-          <button id="three">3</button>
-          <button id="four">4</button>
-          <button id="five">5</button>
-          <button id="six">6</button>
-          <button id="seven">7</button>
-          <button id="eight">8</button>
-          <button id="nine">9</button>
-          <button id="decimal">.</button>
-          <button id="add">+</button>
-          <button id="subtract">-</button>
-          <button id="multiply">x</button>
-          <button id="divide">/</button>
-          <button id="clear">C</button>
-          <button id="equals">=</button>
+        <div id="display">
+          {this.state.firstRow}
+          <br /> {this.state.secondRow}
         </div>
+        <div id="key-wrapper">{buttons}</div>
       </div>
-    )
+    );
   }
 }
 
