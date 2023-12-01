4ec2efcb1dfe34b3d592ed4af140173e9ea8fd51
Beringer Dorottya <beringerdorottya@gmail.com>
2023-12-01 13:35:07 +0100

    added css, improved calculation performance

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-function makeNum(arr) {
+function makeInteger(arr) {
   let num = 0;
   for (let dec = 0; dec < arr.length; dec++) {
     num += arr[arr.length - 1 - dec] * Math.pow(10, dec);
@@ -16,7 +16,7 @@ function computeResult(arr) {
   }
   if (arr.includes('-')) {
     const opIndex = arr.findLastIndex(element => element === '-');
-    if (arr[opIndex - 1] === 'x' || arr[opIndex - 1] === '/') {
+    if (['-', 'x', '/'].includes(arr[opIndex - 1])) {
       const newArr = arr.slice(0, opIndex);
       newArr.push(-computeResult(arr.slice(opIndex + 1)));
       return computeResult(newArr);
@@ -35,9 +35,9 @@ function computeResult(arr) {
     const decimal = arr.findLastIndex(element => element === '.');
     const integerPart = arr.slice(0, decimal);
     const fractionalPart = arr.slice(decimal + 1);
-    return makeNum(integerPart) + makeNum(fractionalPart) * Math.pow(10, 1 + decimal - arr.length);
+    return makeInteger(integerPart) + makeInteger(fractionalPart) * Math.pow(10, 1 + decimal - arr.length);
   }
-  return makeNum(arr);
+  return makeInteger(arr);
 }
 
 function separateLastNumber(arr) {
@@ -49,23 +49,23 @@ function separateLastNumber(arr) {
 }
 
 const buttonConfig = [
-  { id: 'zero', html: 0 },
-  { id: 'one', html: 1 },
-  { id: 'two', html: 2 },
-  { id: 'three', html: 3 },
-  { id: 'four', html: 4 },
-  { id: 'five', html: 5 },
-  { id: 'six', html: 6 },
-  { id: 'seven', html: 7 },
-  { id: 'eight', html: 8 },
-  { id: 'nine', html: 9 },
-  { id: 'decimal', html: '.' },
-  { id: 'add', html: '+' },
-  { id: 'subtract', html: '-' },
-  { id: 'multiply', html: 'x' },
-  { id: 'divide', html: '/' },
-  { id: 'clear', html: 'C' },
-  { id: 'equals', html: '=' }
+  { id: 'zero', html: 0, className: 'number' },
+  { id: 'one', html: 1, className: 'number' },
+  { id: 'two', html: 2, className: 'number' },
+  { id: 'three', html: 3, className: 'number' },
+  { id: 'four', html: 4, className: 'number' },
+  { id: 'five', html: 5, className: 'number' },
+  { id: 'six', html: 6, className: 'number' },
+  { id: 'seven', html: 7, className: 'number' },
+  { id: 'eight', html: 8, className: 'number' },
+  { id: 'nine', html: 9, className: 'number' },
+  { id: 'decimal', html: '.', className: 'decimal' },
+  { id: 'add', html: '+', className: 'operator' },
+  { id: 'subtract', html: '-', className: 'operator' },
+  { id: 'multiply', html: 'x', className: 'operator' },
+  { id: 'divide', html: '/', className: 'operator' },
+  { id: 'equals', html: '=', className: 'equals' },
+  { id: 'clear', html: 'C', className: 'clear' }
 ];
 
 const defaultState = {
@@ -82,11 +82,12 @@ class App extends React.Component {
     this.createButton = this.createButton.bind(this);
     this.handleArr = this.handleArr.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
   }
 
-  createButton({ id, html }) {
+  createButton({ id, html, className }) {
     return (
-      <button id={id} key={id} onClick={() => this.handleClick(html)}>
+      <button id={id} key={id} className={className} onClick={() => this.handleClick(html)}>
         {html}
       </button>
     );
@@ -94,17 +95,24 @@ class App extends React.Component {
 
   handleArr(button, arr) {
     const operators = ['+', '-', 'x', '/'];
-    if (operators.includes(button) && button === arr[arr.length - 1]) {
-      return arr;
-    }
     if (operators.includes(button) && button !== '-' && operators.includes(arr[arr.length - 2]) && arr[arr.length - 1] === '-') {
       //[+-x/], '-', then [+x/] => remove both, add new
+      // this if(...) must be the first to evaluate!
       arr.splice(arr.length - 2, 2, button);
+      return arr;
+    }
+    if (button === '-' && arr[arr.length - 1] === '-' && operators.includes(arr[arr.length - 2])) {
+      //[+x/], '-' then - => do nothing
       return arr;
     }
     if (operators.includes(button) && button !== '-' && operators.includes(arr[arr.length - 1])) {
       //[+-x/] then [+x/] => remove, add new
       arr[arr.length - 1] = button;
+      return arr;
+    }
+    if (operators.includes(button) && arr.length === 0) {
+      //[] then [+-x/] => insert 0, [+-*/]
+      arr.push(0, button);
       return arr;
     }
     if (button === 0 && arr[arr.length - 1] === 0 && (operators.includes(arr[arr.length - 2]) || arr.length === 1)) {
@@ -169,13 +177,39 @@ class App extends React.Component {
     });
   }
 
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown(event) {
+    const validEvents = buttonConfig.map(({ html }) => html);
+    let button = event.key;
+    if (event.key === 'Enter') {
+      button = '=';
+    }
+    if (event.key === 'c') {
+      button = 'C';
+    }
+    if (validEvents.includes(button)) {
+      this.handleClick(button);
+    } else if (validEvents.includes(Number(button))) {
+      this.handleClick(Number(button));
+    }
+  }
+
   render() {
     const buttons = buttonConfig.map(buttonInfo => this.createButton(buttonInfo));
 
     return (
       <div id="calculator">
-        <div id="formula">{this.state.firstRow}</div>
-        <div id="display">{this.state.secondRow}</div>
+        <div id="display-wrapper">
+          <div id="formula">{this.state.firstRow}</div>
+          <div id="display">{this.state.secondRow}</div>
+        </div>
         <div id="key-wrapper">{buttons}</div>
       </div>
     );
